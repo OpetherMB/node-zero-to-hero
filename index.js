42284dbf4954c8ce65a25b6bae3aa15897d0bf1f
kosinix <kosinix@users.noreply.github.com>
2019-06-21 22:27:54 +0800

    Chapter 3

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const session = require('express-session')
 
 const port = 3000
 const dirView = __dirname + '/data/view' // Path to view directory
 
 const app = express()
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
 
 //// Setup view
 // Setup nunjucks loader. See https://mozilla.github.io/nunjucks/api.html#loader
@@ -20,4 +27,16 @@ nunjucksEnv.express(app) // Hook up express and nunjucks
 app.get('/', (req, res) => {
     res.render('index.html')
 })
+
+app.get('/secured', (req, res) => {
+    if (!req.session.login) {
+        return res.redirect('/login')
+    }
+    res.render('secured.html')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.html')
+})
+
 app.listen(port, () => console.log(`App running on http://localhost:${port}`))
